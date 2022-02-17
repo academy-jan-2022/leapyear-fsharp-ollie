0fbcd26d7d48ffc7ce58cbaa026e2c0a2a9edf51
iagardi <garbigz@gmail.com>
2022-02-17 12:04:34 +0000

    green

--- LeapYearFSharp/LeapYear.fs
+++ LeapYearFSharp/LeapYear.fs
@@ -1,4 +1,17 @@
 
 
-let Check num =
-    num % 4 = 0 
+let isDivisibleBy divisor num =
+    num % divisor = 0
+
+let divisibleByFour num =
+    isDivisibleBy 4 num
+    
+let divisibleByOneHundred num=
+    isDivisibleBy 100 num
+    
+let divisibleByFourHundred num =
+    isDivisibleBy 400 num
+    
+let Check num = 
+   divisibleByFour num && not (divisibleByOneHundred num) || ( divisibleByFourHundred num)  
+   

d1641fbb13f7f0a065d859a3c55b841786344459
iagardi <garbigz@gmail.com>
2022-02-17 11:34:38 +0000

    red

--- LeapYearFSharp.Tests/Tests.fs
+++ LeapYearFSharp.Tests/Tests.fs
@@ -8,6 +8,7 @@ open Xunit
 [<InlineData(2, false)>]
 [<InlineData(3, false)>]
 [<InlineData(4, true)>]
+[<InlineData(8, true)>]
 let ``Check is leap year`` input expected =
     let result = LeapYear.Check input
     Assert.Equal(expected, result)
